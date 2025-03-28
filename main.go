1245144c9410d522a01c3f3f170781b7a2eacf00
AnatolyUss <anatolyuss@gmail.com>
2025-03-28 23:23:14 +0300

    run sm-2 in different modes

--- main.go
+++ main.go
@@ -2,65 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"log"
-	"net/http"
-	"sm-2/main/internal/lib/dto"
-
-	"github.com/gin-gonic/gin"
+	"os"
+	"sm-2/main/cmd"
 )
 
 func main() {
-	greeting := "Hello, sm-2!"
-	fmt.Println(greeting)
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env vars: %+v", err)
+	}
 
-	log.SetPrefix("sm-2: ")
-	log.SetFlags(0)
+	mode := os.Args[1]
 
-	router := gin.Default()
-	router.GET("/healthcheck", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
+	if mode == "--init-db" {
+		fmt.Printf("Mode: %s\n", mode)
+		os.Exit(0)
+		//
+	} else if mode == "--run-api" {
+		fmt.Printf("Mode: %s\n", mode)
+		port := os.Getenv("HTTP_PORT")
 
-	router.POST("/model", func(c *gin.Context) {
-		var modelDto dto.ModelDto
-
-		if err := c.ShouldBindJSON(&modelDto); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"Model validation failed": err.Error()})
-			return
+		if port == "" {
+			port = "8080"
 		}
 
-		// const modelService = new ModelService();
-		// await modelService.saveModel(modelDto);
-		// response.status(201).json(new ResponseDto('Ok'));
+		fmt.Printf("HTTP_PORT: %v\n", port)
+		cmd.RunApi(":" + port)
+	} else {
+		fmt.Printf("Invalid mode: %s\n", mode)
+		os.Exit(1)
+	}
 
-		c.JSON(http.StatusOK, gin.H{"status": "Good model!"})
-	})
-
-	router.POST("/request", func(c *gin.Context) {
-		var requestDto dto.RequestDto
-
-		if err := c.ShouldBindJSON(&requestDto); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"Request validation failed": err.Error()})
-			return
-		}
-
-		// const modelService = new ModelService();
-		//        const modelDto = await modelService.getModel(requestDto.path, requestDto.method);
-		//
-		//        if (!modelDto) {
-		//          const msg = `Model not found for path:method "${requestDto.path}:${requestDto.method}"`;
-		//          response.status(404).json(new ResponseDto(msg));
-		//          return;
-		//        }
-		//
-		//        const requestService = new RequestService();
-		//        const validationResponse = requestService.validateRequest(requestDto, modelDto);
-		//
-		//        // Return status 200 instead of 201 as per explicit request at the task definition.
-		//        response.status(200).json(new ResponseDto(validationResponse));
-
-		c.JSON(http.StatusOK, gin.H{"status": "Good request!"})
-	})
-
-	router.Run() // listen and serve on 0.0.0.0:8080
+	//
 }
